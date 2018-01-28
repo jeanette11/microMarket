53fea480a1137e2cfa396774a8d6b519c6c7f39d
Elmer <infcle@gmail.com>
2018-01-28 12:13:52 -0400

    ejemplo de envio de datos

--- public/views/template/contenido.php
+++ public/views/template/contenido.php
@@ -8,7 +8,23 @@
         </div>
         <div class="col-xs-1 col-sm-2 col-md-3 col-lg-3">
         </div>
-        
+    </div>
+    <div class="row">
+    	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+    		<form action="" method="POST" class="form-horizontal" role="form">
+    				<div class="form-group">
+    					<legend>envio de datos</legend>
+    				</div>
+    		
+    				<input type="text" name="nombre" id="inputNombre" class="form-control" value="" required="required" pattern="" title="">
+    		
+    				<div class="form-group">
+    					<div class="col-sm-10 col-sm-offset-2">
+    						<button type="submit" class="btn btn-primary">Submit</button>
+    					</div>
+    				</div>
+    		</form>
+    	</div>
     </div>
 </div>
 <!--body wrapper end-->
