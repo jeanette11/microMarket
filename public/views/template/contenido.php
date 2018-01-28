9541a0fd7c9e860e0681c0cc9e6df5abbef1f710
Elmer <infcle@gmail.com>
2018-01-28 14:52:25 -0400

    volvianedo al estado anterior del proyecto

--- public/views/template/contenido.php
+++ public/views/template/contenido.php
@@ -9,44 +9,5 @@
         <div class="col-xs-1 col-sm-2 col-md-3 col-lg-3">
         </div>
     </div>
-    <div class="row">
-    	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-    		<form class="form-horizontal" role="form" id="miform" name="miform">
-				<div class="form-group">
-					<legend>envio de datos</legend>
-				</div>
-		
-				<input type="text" name="nombre" id="inputNombre" class="form-control">
-		
-				<div class="form-group">
-					<div class="col-sm-10 col-sm-offset-2">
-						<button type="button" class="btn btn-primary" id="enviar">Enviar</button>
-					</div>
-				</div>
-    		</form>
-    	</div>
-    </div>
 </div>
 <!--body wrapper end-->
-
-<script>
-	$(document).ready(function() {
-		$('#enviar').click(function(event) {
-			$.ajax({
-				url: '../../app/models/prueba.php',
-				type: 'post',
-				data: $('#miform').serialize(),
-			})
-			.done(function() {
-				console.log("success");
-			})
-			.fail(function() {
-				console.log("error");
-			})
-			.always(function() {
-				console.log("complete");
-			});
-			
-		});
-	});
-</script>
