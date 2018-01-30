652fbef53715d77a414aab27673fb1c2f5867685
Elmer Coronel <infcle@gmail.com>
2018-01-30 10:54:07 -0400

    Menu de usuario logeado

--- public/views/template/menu_right.php
+++ public/views/template/menu_right.php
@@ -3,13 +3,13 @@
         <li>
             <a href="#" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
                 <img src="<?php echo ROOT; ?>resources/assets/images/photos/user-avatar.png" alt="" />
-                John Doe
+                <?php  ?>Nombre de usuario
                 <span class="caret"></span>
             </a>
             <ul class="dropdown-menu dropdown-menu-usermenu pull-right">
-                <li><a href="#"><i class="fa fa-user"></i>  Profile</a></li>
-                <li><a href="#"><i class="fa fa-cog"></i>  Settings</a></li>
-                <li><a href="#"><i class="fa fa-sign-out"></i> Log Out</a></li>
+                <li><a href="#"><i class="fa fa-user"></i>  Perfil</a></li>
+                <li><a href="#"><i class="fa fa-cog"></i>  Configuraciones</a></li>
+                <li><a href="#"><i class="fa fa-sign-out"></i> Salir</a></li>
             </ul>
         </li>
     </ul>
