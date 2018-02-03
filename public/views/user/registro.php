0ef2eae7e26b5ff375016ae9aef0467003f0ef79
sergui <juansergio1981@gmail.com>
2018-02-02 22:46:58 -0400

    ejemplo validacion

--- public/views/user/registro.php
+++ public/views/user/registro.php
@@ -47,6 +47,32 @@
     $(document).ready(function() {
         $("#frmUsuario").validate({
             debug:true,
+            rules: {
+                firstname: "required",
+                lastname: "required",
+                username: {
+                    required: true,
+                    minlength: 2
+                },
+                password: {
+                    required: true,
+                    minlength: 5
+                },
+                confirm_password: {
+                    required: true,
+                    minlength: 5,
+                    equalTo: "#password"
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                topic: {
+                    required: "#newsletter:checked",
+                    minlength: 2
+                },
+                agree: "required"
+            },
             submitHandler: function (form) {
                 $.ajax({
                     url: '../../models/user/registro_model.php',
