66d7a805c978ef4383d0bc717cf873428e797366
Jane <jeaneinfo@gmail.com>
2018-02-08 16:49:42 -0400

    ventas

--- public/views/ventas/index.php
+++ public/views/ventas/index.php
@@ -43,6 +43,7 @@
                                     </div>
                                     </div>
                             </div>
+                            <div class=""></div>
                         </form>
                     </div>
 
